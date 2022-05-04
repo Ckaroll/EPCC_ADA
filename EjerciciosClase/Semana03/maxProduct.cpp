55053b4d76448ec9918d632513a0cea272bc486a
Ckaroll <64216012+Ckaroll@users.noreply.github.com>
2022-05-03 22:18:16 -0500

    Create maxProduct.cpp

---
+++ EjerciciosClase/Semana03/maxProduct.cpp
@@ -0,0 +1,4 @@
+//Dada una secuencia de n números no negativos y mayores que cero.
+//El objetivo es encontrar el mayor número que se pueda obtener
+//multiplicando dos números de esta secuencia.
+// En tiempo lineal
